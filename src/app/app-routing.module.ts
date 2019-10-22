f315c6ea894573e9dc489f6c1218182dcb537491
roman120487 <roman120487@ukr.net>
2019-10-22 14:14:31 +0300

    menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MainComponent } from './main/main.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { BrendsComponent } from './brends/brends.component';
+import { AdminComponent } from './admin/admin.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'main', component: MainComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'brends', component: BrendsComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'admin', component: AdminComponent },
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
