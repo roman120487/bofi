1ae3d5ee8475c63988eae430fc2e8fb3dd729187
roman120487 <roman120487@ukr.net>
2019-11-26 11:57:16 +0200

    footer complate

--- src/app/reference/jeven/jeven.component.ts
+++ src/app/reference/jeven/jeven.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jeven.component.css']
 })
 export class JevenComponent implements OnInit {
-
+  doc1: boolean;
+  doc2: boolean;
+  doc3: boolean;
   constructor() { }
 
   ngOnInit() {
   }
+
+  showDoc(val: string): void {
+    console.log(val);
+    if (val === 'doc1') {
+      this.doc1 = true;
+      this.doc2 = false;
+      this.doc3 = false;
+    } else if (val === 'doc2') {
+      this.doc1 = false;
+      this.doc2 = true;
+      this.doc3 = false;
+    } else if (val === 'doc3') {
+      this.doc1 = false;
+      this.doc2 = false;
+      this.doc3 = true;
+    }
+    console.log(val);
+  }
+
+
 
 }
